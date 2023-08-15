9899de20295a2268df4a02496a179384ea617d54
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-08-15 22:26:38 +0100

    changed page copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,26 +16,26 @@ const Home = () => {
         <title>Listado de los mejores softwares</title>
       </Head>
       <Navbar />
-      <main className="">
+      <main>
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-4xl">
             <div className="text-center">
-              <h1 className="text-6xl font-bold tracking-tight text-gray-900 sm:text-8xl">
-                Encuentra el mejor Software para tu Negocio
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-8xl">
+                Cual es la mejor aplicación para tu empresa?
               </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                Explora y compra software en cuestion de minutos sin tener que ser experto.
+              <p className="max-w-4xl mt-6 text-lg leading-8 text-gray-600">
+                Nuestro sistema de busqueda® te permitirá encontrar la solución que mejor se adapte a tu negocio.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
                   href="#"
-                  className="rounded-md bg-fuchsia-950 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Explorar
+                  Buscar
                 </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                <Link href="/review" className="text-sm font-semibold leading-6 text-gray-900">
                   Opinar <span aria-hidden="true">→</span>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
