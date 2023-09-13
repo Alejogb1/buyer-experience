20f96d779555df4966e7c62d4967e33f9655f0f1
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-09-13 15:09:36 +0100

    Update send.ts

--- src/server/api/send.ts
+++ src/server/api/send.ts
@@ -7,9 +7,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const SendEmailRequest = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         const data = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
-            to: ['delivered@resend.dev'],
-            subject: 'Hello world',
+            from: '<onboarding@audiencia.dev>',
+            to: ['marcos@salesforce.com'],
+            subject: 'Zenrows',
             react: EmailTemplate({ Name: 'John' }),
         });
 
