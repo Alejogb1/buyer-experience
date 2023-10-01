356f3e92cb9680cff4a0c7bb0ba9a8274cb6d0ca
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-10-01 14:31:43 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
+ * @type {import('next').NextConfig}
  */
-await import("./src/env.mjs");
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
 
-/** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
-};
-
-export default config;
+export default nextConfig
