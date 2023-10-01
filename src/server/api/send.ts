3970c3bdb681f9bbf9a383d3ec9463e39d7e955f
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-10-01 14:13:34 +0100

    fix build errors

--- src/server/api/send.ts
+++ src/server/api/send.ts
@@ -1,20 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '../../components/EmailTemplate';
-import { Resend } from 'resend';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import { EmailTemplate } from '../../components/EmailTemplate';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const SendEmailRequest = async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        const data = await resend.emails.send({
-            from: '<onboarding@audiencia.dev>',
-            to: ['marcos@salesforce.com'],
-            subject: 'Zenrows',
-            react: EmailTemplate({ Name: 'John' }),
-        });
+// export const SendEmailRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+//     try {
+//         const data
 
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-};
+//         res.status(200).json(data);
+//     } catch (error) {
+//         res.status(400).json(error);
+//     }
+// };
