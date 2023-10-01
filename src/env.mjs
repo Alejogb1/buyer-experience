8ee128f944c4ba5ddccb790a6f938bd76b00a054
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-10-01 14:20:20 +0100

    fix config

--- src/env.mjs
+++ src/env.mjs
@@ -27,6 +27,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
