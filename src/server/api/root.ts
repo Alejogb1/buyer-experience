540419ea4a5c061cc6096ce2f8e888c9743d1b00
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-10-02 22:18:27 +0100

    next auth env error

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,10 @@ import { createTRPCRouter } from "~/server/api/trpc";
 import { categoryRouter } from "~/server/api/routers/categories";
 import { productRouter } from "~/server/api/routers/products";
 import { reactHookFormRouter } from "~/server/api/routers/form"
+import { initTRPC, TRPCError } from '@trpc/server';
+
+const t = initTRPC.create();
+
 /**
  * This is the primary router for your server.
  *
@@ -11,6 +15,15 @@ export const appRouter = createTRPCRouter({
   category: categoryRouter,
   product: productRouter,
   form: reactHookFormRouter,
+  hello: t.procedure.query(() => {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'An unexpected error occurred, please try again later.',
+      // optional: pass the original error to retain stack trace
+      cause: theError,
+    });
+  }),
+
 });
 
 // export type definition of API
