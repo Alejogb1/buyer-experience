7fe9b2b33ed85799d964fa3e5cd0aab45bc60013
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-10-05 17:47:22 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
               </p>
               <div className="max-w-2xl mt-5 flex justify-left gap-x-6">
                 <Link
-                  href="/explorar"
+                  href="/categories"
                   className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Buscar soluciones
