62fb9223be23fb681e26f08832bb2f220196b412
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-10-06 16:06:27 -0300

    Actualizar navbar.tsx

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -21,14 +21,13 @@ const Navbar = () => {
           <span className="flex items-center text-sm font-medium text-gray-800">
             <span className="flex w-2.5 h-2.5 bg-blue-600 rounded-full mr-1.5 flex-shrink-0"></span>1 viendo</span>
         </div>
-        <div className="flex h-16 items-center justify-between mr-10">
           <Link
             className="text-sm font-medium flex pt-1"
             href="/"
           >
             AUDIENCIA
           </Link>
-        </div>
+      
 
         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static">
           <Link href="/categories" className="hover:underline">
