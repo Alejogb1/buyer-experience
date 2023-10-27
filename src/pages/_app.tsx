326f4702342d0a3d78ae5f8cb655f3de9b489869
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-10-27 19:56:28 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,11 +16,11 @@ const MyApp: AppType<{ session: null }> = ({
       <Head>
           <Script id="google-tag-manager">
             {`
-              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5Z53CSJQ');</script>
+              })(window,document,'script','dataLayer','GTM-5Z53CSJQ');
           `}
           </Script>
         <link rel="icon" href="/favicon.ico" sizes="any" />
@@ -32,14 +32,6 @@ const MyApp: AppType<{ session: null }> = ({
           lang="es"
         />
       </Head>
-      <Script>
-            {`
-              <!-- Google Tag Manager (noscript) -->
-              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Z53CSJQ"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-              <!-- End Google Tag Manager (noscript) -->
-          `}
-          </Script>
       <div className="container">
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-J531CF9875" />
         <Script defer id="google-analytics">
