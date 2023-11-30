f190b59e588958582ea380d0ffe6b2aea4163610
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-11-30 12:23:44 -0300

    subdomain routing

---
+++ src/pages/s/salesmeetings/index.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Salesmeetings() {
+    return (
+        <>
+           <h1 className="text-xl">Company name:</h1>
+           <p>Salesmeetings</p>
+        </>
+    );
+}
