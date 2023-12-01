cec38befda6f8f368a3c79b948559d1b7921f874
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-12-01 12:42:29 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,18 +11,5 @@ export async function middleware(req: NextRequest) {
 
   const host = req.headers.get('host');
   const subdomain = getValidSubdomain(host);
-  try {
-    if (subdomain) {
-      if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return;
-      // Subdomain available, rewriting
-      console.log(`>>> Rewriting: ${url.pathname} to ${subdomain}${url.pathname}`);
-      url.pathname = `/s/${subdomain}${url.pathname}`;
-      return NextResponse.rewrite(url);
-    } 
-  } catch (error) {
-    return { message: 'Failed subdomain validation ',error };
-  }
 
-  url.pathname = url.pathname
-  return NextResponse.next()
 }
