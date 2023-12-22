fcfd7030d606ce9eebf58d7f746b71c651e3327d
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-12-22 17:09:02 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,19 +10,17 @@ export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const host = req.headers.get('host');
   const subdomain = getValidSubdomain(host);
+  console.log("attributes: ", url.pathname,)
     if (subdomain) {
       if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return;
       // Subdomain available, rewriting
-      console.log(`>>> Rewriting: ${url.pathname} to /${subdomain}${url.pathname}`); 
+      console.log(`>>> Rewriting: ${url.pathname} to /s/${subdomain}${url.pathname}`); 
       if (host && host.includes('salesmeetings') && url.pathname == "/") {
         console.log(1)
         url.pathname = `/s/${subdomain}${url.pathname}`
+        console.log("new pathname: ", `/s/${subdomain}${url.pathname}`)
         return NextResponse.rewrite(url);
-      } else if (host && host.includes('salesmeetings') ) {
-        console.log(2)
-          url.pathname = `/s/${subdomain}${url.pathname}`
-          return NextResponse.rewrite(url);
-      }
+      } 
       else {
         console.log("next response")
         return NextResponse.rewrite(req.nextUrl);
