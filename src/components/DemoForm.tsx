b36c31c0e535a340479a7d1bbf6efce342491a95
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-12-28 21:25:39 -0300

    fixed dark for

--- src/components/DemoForm.tsx
+++ src/components/DemoForm.tsx
@@ -47,7 +47,7 @@ export default function DemoForm () {
                   <div className="mb-5">  
                     <label className="block mb-2 text-sm font-medium text-gray-900">Nombre completo
                         <br/>
-                        <input {...form.register('name')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 " required/>
+                        <input {...form.register('name')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
                     </label>
                     {form.formState.errors.name?.message && (
                             <p className="text-red-500 text-xs">
--- src/components/PricingComponent.tsx
+++ src/components/PricingComponent.tsx
@@ -2,21 +2,21 @@ import Image from "next/image"
 import ModalForm from "./ModalForm"
 export default function PricingComponent (){
     return(
-        <section className="bg-white">
+        <section className="bg-white -900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6" id="planes">
             <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-                <h2 className="mb-4 text-4xl tracking-tight font-bold text-black">Un plan simple</h2>
+                <h2 className="mb-4 text-4xl tracking-tight font-bold text-black ">Un plan simple</h2>
             </div>
             <div className="space-y-8 lg:grid lg:grid-cols-2 sm:gap-2 xl:gap-6 lg:space-y-0 max-w-2xl m-auto">
-                <div className="flex flex-col p-6 mx-auto max-w-xs text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow border-gray-600 xl:p-8 bg-gray-800 text-white">
+                <div className="flex flex-col p-6 mx-auto max-w-xs text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow -600 xl:p-8 -800 ">
                     <h3 className="mb-4 text-2xl font-bold text-left">Gratis</h3>
                     <div className="flex justify-left items-baseline my-8">
                         <span className="mr-2 text-5xl font-bold">$0</span>
-                        <span className="text-gray-500">/mes</span>
+                        <span className="text-gray-500 -400">/mes</span>
                     </div>
                     <ul role="list" className="mb-8 space-y-4 text-left">
                         <li className="flex items-center space-x-3">
-                            <svg className="flex-shrink-0 w-5 h-5 text-green-500 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                            <svg className="flex-shrink-0 w-5 h-5 text-green-500 -400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                             <span>Configuración individual</span>
                         </li>
                         <li className="flex items-center space-x-3">
@@ -38,24 +38,24 @@ export default function PricingComponent (){
                     </ul>
                     <ModalForm/>
                 </div>
-                <div className="flex flex-col p-6 mx-auto max-w-xs text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow border-gray-600 xl:p-8 bg-gray-800 text-white">
+                <div className="flex flex-col p-6 mx-auto max-w-xs text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow -600 xl:p-8 -800 ">
                     <h3 className="mb-4 text-2xl font-semibold text-left">Pago</h3>
                     <div className="flex justify-left items-baseline my-8">
                         <span className="mr-2 text-5xl font-bold">$150</span>
-                        <span className="text-gray-500">/mes</span>
+                        <span className="text-gray-500 -400">/mes</span>
                     </div>
                     <ul role="list" className="mb-8 space-y-4 text-left">
                         <li className="flex items-center space-x-3">
-                            <svg className="flex-shrink-0 w-5 h-5 text-green-500 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                            <svg className="flex-shrink-0 w-5 h-5 text-green-500 -400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                             <span>Configuración individual</span>
                         </li>
                         <li className="flex items-center space-x-3">
-                            <svg className="flex-shrink-0 w-5 h-5 text-green-500 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                            <svg className="flex-shrink-0 w-5 h-5 text-green-500 -400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                             <span>Datos ilimitados (almacenamiento)</span>
                         </li>
                 
                         <li className="flex items-center space-x-3">
-                            <svg className="flex-shrink-0 w-5 h-5 text-green-500 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                            <svg className="flex-shrink-0 w-5 h-5 text-green-500 -400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                             <span>Empresas identificadas: <span className="font-semibold">ilimitadas</span></span>
                         </li>
                     </ul>
