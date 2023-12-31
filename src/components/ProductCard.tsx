012a127fffa8727fe4b112b7457085210b8730fc
Alejo Garcia <52103963+Alejogb1@users.noreply.github.com>
2023-12-31 02:11:56 -0300

    Link issue

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -21,36 +21,38 @@ export default function ProductCard(product: ProductCardProps) {
         <Link href={{
             pathname: '/p/[product]',
             query: { product: name },
-        }} className="lg:w-4/12 w-8/12 gap-4 flex-start flex flex-row group cursor-pointer">
-            <figure className=" overflow-hidden shrink-0 relative">
-                <img
-                    className="rounded-md block relative shrink-0 w-16"
-                    src={logo}
-                    alt={name}
-                />
-            </figure>
-            <div className="flex flex-col flex-start gap-1">
-                <div className="">
-                    <p className='group-hover:underline text-md text-black font-semibold'>
-                        {name}
-                    </p>
-                </div>
+        }} >
+            <div className="gap-4 flex-start flex flex-row group cursor-pointer">
+                <figure className="overflow-hidden shrink-0 relative">
+                    <img
+                        className="rounded-md block relative shrink-0 w-12"
+                        src={logo}
+                        alt={name}
+                    />
+                </figure>
+                <div className="flex flex-col flex-start gap-1">
+                    <div className="">
+                        <p className='group-hover:underline text-md text-black font-semibold'>
+                            {name}
+                        </p>
+                    </div>
 
-                <div className="self-stretch items-center flex flex-row relative text-sm text-black font-normal">
-                    <span className="">
-                        {rating}
-                    </span>
-                    <span className="shrink-0 mr-2">
-                        <Image
-                            width={12}
-                            height={100}
-                            src="/starIcon.png" alt=""
-                        />
-                    </span>
-                    <span> </span>
-                    <span className="overflow-hidden whitespace-nowrap overflow-ellipsis"> Plan gratis disponible</span>
+                    <div className="self-stretch items-center flex flex-row relative text-sm text-black font-normal">
+                        <span className="">
+                            {rating}
+                        </span>
+                        <span className="shrink-0 mr-2">
+                            <Image
+                                width={12}
+                                height={12}
+                                src="/starIcon.png" alt=""
+                            />
+                        </span>
+                        <span> </span>
+                        <span className="overflow-hidden whitespace-nowrap overflow-ellipsis"> Plan gratis disponible</span>
+                    </div>
+                    <div ><p className="text-sm text-gray-500 font-normal">{description}</p></div>
                 </div>
-                <div ><p className="text-sm text-gray-500 font-normal">{description}</p></div>
             </div>
         </Link>
     )
